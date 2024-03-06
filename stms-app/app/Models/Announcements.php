af9974974d741feb885a9511155ef51b2a2c67c6
rwaah <412217578@qu.edu.sa>
2024-03-07 00:57:30 +0300

    Add seeders files to insert data into DB when migrate

--- stms-app/app/Models/Announcements.php
+++ stms-app/app/Models/Announcements.php
@@ -16,4 +16,8 @@ class Announcements extends Model
         'created_at',
         'updated_at'
     ];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
